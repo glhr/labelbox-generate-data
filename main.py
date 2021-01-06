9aeefc4316d2aecbedc77d19796f5d0540553d4a
glhr <grenblot@gmail.com>
2021-01-06 22:26:20 +0100

    fixed paths and labelbox urls

--- main.py
+++ main.py
@@ -26,31 +26,47 @@ for directory in [OUTPUT_COLOR_CORRECTED_FOLDER,OUTPUT_LABELED_FOLDER,OUTPUT_ORI
       pass # directory already exists
 
 
-for path in glob.iglob(os.path.join(INPUT_JSON_FOLDER,'*.json')):
-    with open(path) as json_file:
-        data = json.load(json_file)
-        for i in range(len(data)):
-            try:
-              filename = data[i]['External ID']
-              orig_url = data[i]['Labeled Data']
-              url = data[i]['Label']['objects'][0]['instanceURI']
-              #print(url)
-              res_label = requests.get(url)
-              res_label.raise_for_status()
-              open(os.path.join(OUTPUT_LABELED_FOLDER, filename), 'wb').write(res_label.content)
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjazJxNnpyNGNpODczMDgzODgydzBtMzI5Iiwib3JnYW5pemF0aW9uSWQiOiJjazJxNnpyM25pODZ4MDgzOHF4MnB2cW9nIiwiYXBpS2V5SWQiOiJja2psc3VjdmZoN2o4MDg5MmhxbHYxdzVyIiwiaWF0IjoxNjA5OTYwMzgxLCJleHAiOjIyNDExMTIzODF9.B-nk_NVBo_KxMKlbbf-EIh0dpY8bvVxjqFpMCBCGaUk"
 
-              res_orig = requests.get(orig_url)
-              res_orig.raise_for_status()
-              open(os.path.join(OUTPUT_ORIGINAL_FOLDER,filename), 'wb').write(res_orig.content)
+# for path in glob.iglob(os.path.join(INPUT_JSON_FOLDER,'*.json')):
+#     with open(path) as json_file:
+#         data = json.load(json_file)
+#         for i in range(len(data)):
+#             try:
+#               filename = data[i]['External ID']
+#               orig_url = data[i]['Labeled Data']
+#               url = data[i]['Label']['objects'][0]['instanceURI']
+#               #print(url)
+#               if "&token=" in url:
+#                   url = "".join(url.split("&token=")[:-1])
+#                   url = url+'&token='+key
+#               elif "?token=" in url:
+#                   url = "".join(url.split("?token=")[:-1])
+#                   url = url+'?token='+key
+#               res_label = requests.get(url)
+#               res_label.raise_for_status()
+#               open(os.path.join(OUTPUT_LABELED_FOLDER, filename), 'wb').write(res_label.content)
+#
+#               if "&token=" in orig_url:
+#                   orig_url = "".join(orig_url.split("&token=")[:-1])
+#                   orig_url = orig_url+'&token='+key
+#               elif "?token=" in orig_url:
+#                   orig_url = "".join(orig_url.split("?token=")[:-1])
+#                   orig_url = orig_url+'?token='+key
+#               res_orig = requests.get(orig_url)
+#               res_orig.raise_for_status()
+#               open(os.path.join(OUTPUT_ORIGINAL_FOLDER,filename), 'wb').write(res_orig.content)
 
+for path in glob.iglob(os.path.join(OUTPUT_LABELED_FOLDER,'*.png')):
+              filename = 'labeled_'+path.split("/")[-1]
               input_line = os.path.join('training',OUTPUT_ORIGINAL_FOLDER,filename)
               print(input_line)
 
-              img = cv2.imread(os.path.join(OUTPUT_ORIGINAL_FOLDER,filename))
+              img = cv2.imread(path)
 
               # Mask image to only select white
-              white=np.array([255,255,255])
-              mask=cv2.inRange(img,white,white)
+              white=np.array([200,200,200])
+              mask=cv2.inRange(img,white,np.array([255,255,255]))
               #mask_inv = [not m for m in mask]
 
               # Change image colors
@@ -59,12 +75,10 @@ for path in glob.iglob(os.path.join(INPUT_JSON_FOLDER,'*.json')):
 
               path = path.split('/')[-1]
               cv2.imwrite(os.path.join(OUTPUT_COLOR_CORRECTED_FOLDER,filename),img)
-              output_line = os.path.join('training',OUTPUT_COLOR_CORRECTED_FOLDER,'labeled_'+filename)
+              output_line = os.path.join('training',OUTPUT_COLOR_CORRECTED_FOLDER,filename)
               print(output_line)
 
               training += input_line + ' ' + output_line + '\n'
-            except KeyError:
-              pass
 
 # write input and output file paths to file for training
 with open(os.path.join("output","training.txt"), "w+") as training_file:
