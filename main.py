7a4859a7bcd6e4a59e267b55cc0c50920b6c6cfb
Fábio Morais <fmorais@abyssal.eu>
2021-03-28 20:21:22 +0100

    added loading bar

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import glob
 import json
 import requests
 import os
+import time
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--fetch', action="store_true", default="false")
 args, unknown = parser.parse_known_args()
 training = ""
+BACKGROUND_COLOR = (0,0,0) #black
 
 INPUT_JSON_FOLDER = 'input'
 OUTPUT_COLOR_CORRECTED_FOLDER = 'output/images_color_corrected'
@@ -24,6 +26,30 @@ def hex_to_rgb(v):
     h = v.lstrip('#')
     return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
 
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
+    
 # create output directories if needed
 for directory in [OUTPUT_COLOR_CORRECTED_FOLDER,OUTPUT_LABELED_FOLDER,OUTPUT_ORIGINAL_FOLDER]:
   try:
@@ -32,12 +58,16 @@ for directory in [OUTPUT_COLOR_CORRECTED_FOLDER,OUTPUT_LABELED_FOLDER,OUTPUT_ORI
       pass # directory already exists
 
 
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjazJxNnpyNGNpODczMDgzODgydzBtMzI5Iiwib3JnYW5pemF0aW9uSWQiOiJjazJxNnpyM25pODZ4MDgzOHF4MnB2cW9nIiwiYXBpS2V5SWQiOiJja2psc3VjdmZoN2o4MDg5MmhxbHYxdzVyIiwiaWF0IjoxNjA5OTYwMzgxLCJleHAiOjIyNDExMTIzODF9.B-nk_NVBo_KxMKlbbf-EIh0dpY8bvVxjqFpMCBCGaUk"
+
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJja21tOWVpc3IyMXg4MDcxNHNsZGgwdWNkIiwib3JnYW5pemF0aW9uSWQiOiJja21tOWVpczdvMWRzMDc3MjhvZmIyNWhzIiwiYXBpS2V5SWQiOiJja21yanVhb3F6emM0MDc5MDJxZzk3eWo1IiwiaWF0IjoxNjE2ODM4NDA2LCJleHAiOjIyNDc5OTA0MDZ9.RmY5u0gWbfbgPEDsc_66a7_aeraDe5gPpFpm-iVDWlU"
 if args.fetch:
     for path in glob.iglob(os.path.join(INPUT_JSON_FOLDER,'*.json')):
         with open(path) as json_file:
             data = json.load(json_file)
+            print("Downloading the labels and the original files")
+            printProgressBar(0, 100, prefix = 'Progress:', suffix = 'Complete', length = 50)
             for i in range(len(data)):
+                printProgressBar(i + 1, len(data), prefix = 'Progress:', suffix = 'Complete', length = 50)
                 filename = data[i]['External ID']
                 orig_url = data[i]['Labeled Data']
                 aux = [x for x in filename.split('.') if x.strip()]
@@ -45,7 +75,7 @@ if args.fetch:
                     paths.append(aux[0])# name of the file without the .png
                     os.mkdir(os.path.join(OUTPUT_LABELED_FOLDER, str(aux[0])))#create folder with name of filename
                 except OSError:
-                    print ("Creation of the directory %s failed" % str(aux[0]))
+                    pass
                 colorsByImage[aux[0]]=[]
                 for h in range(len(data[i]['Label']['objects'])):
                     try:
@@ -74,11 +104,11 @@ if args.fetch:
                     except :
                         pass
 
-
+print("\nGenerating the labels corrected")
+printProgressBar(0, 100, prefix = 'Progress:', suffix = 'Complete', length = 50)
 for i in range(len(paths)):
+    printProgressBar((i+1), len(paths), prefix = 'Progress:', suffix = 'Complete', length = 50)
     tifCounter = len(glob.glob1(str(os.path.join(OUTPUT_LABELED_FOLDER, str(paths[i]))),"*.png"))
-    print (tifCounter)
-    print (paths[i])
     filename = 'labeled_'+paths[i]+".png"
     input_line = os.path.join('training',OUTPUT_ORIGINAL_FOLDER, str(paths[i])+".png")
     output_line = os.path.join('training',OUTPUT_COLOR_CORRECTED_FOLDER,filename)
@@ -112,6 +142,7 @@ for i in range(len(paths)):
     training += input_line + ' ' + output_line + '\n'
     cv2.imwrite(os.path.join(OUTPUT_COLOR_CORRECTED_FOLDER,filename), added_image)
 
+print("\nCompleted!")
 # write input and output file paths to file for training
 with open(os.path.join("output","training.txt"), "w+") as training_file:
     training_file.write(training)
